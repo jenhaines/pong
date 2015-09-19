92a23f8c6cd320a88387564c5061a008610287d4
Jen Haines <motochick67@gmail.com>
2015-09-18 17:08:55 -0700

    Ball Bounces

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,14 @@ var animate = window.requestAnimationFrame ||
    function(callback) { window.setTimeout(callback, 1000/60)};
 
 var c = document.getElementById('canvas');
-var width = 600;
-var height = 500;
+var width = 800;
+var height = 600;
 c.width = width;
 c.height = height; 
 var ctx = c.getContext('2d'); 
 var playerone = new PlayerOne();
 var playertwo = new PlayerTwo();
-var ball = new Ball(300, 250);
+var ball = new Ball(400, 300);
 
 var keysDown = {};
 
@@ -25,7 +25,7 @@ var render = function(){
   ctx.fillStyle = "#339966";
   ctx.fillRect(0, 0, width, height);
   ctx.fillStyle = "#FFF"
-  ctx.fillRect(300, 10, 4, 480);
+  ctx.fillRect(400, 10, 4, 580);
   playerone.render();
   playertwo.render();
   ball.render();
@@ -65,18 +65,18 @@ Paddle.prototype.move = function(x, y) {
     if(this.y < 0) {
       this.y = 0;
       this.y_speed = 0;
-    } else if (this.y + this.height > 500) {
-      this.y = 500 - this.height;
+    } else if (this.y + this.height > 600) {
+      this.y = 600 - this.height;
       this.y_speed = 0;
     }
   };
 
  function PlayerOne(){
-   this.paddle = new Paddle(40, 250, 10, 80);
+   this.paddle = new Paddle(50, 250, 10, 80);
  }
 
  function PlayerTwo(){
-   this.paddle = new Paddle(550, 250, 10, 80);
+   this.paddle = new Paddle(750, 250, 10, 80);
  }
 
  PlayerOne.prototype.render = function(){
@@ -118,8 +118,8 @@ Paddle.prototype.move = function(x, y) {
    this.y = y;
    this.x_speed = 3;
    this.y_speed = 1;
-   this.width = 8;
-   this.height = 8;
+   this.width = 10;
+   this.height = 10;
  };
 
  Ball.prototype.render = function(){
@@ -132,34 +132,33 @@ Paddle.prototype.move = function(x, y) {
  Ball.prototype.update = function(paddle1, paddle2){
   this.x += this.x_speed;
   this.y += this.y_speed;
-  var top_x = this.x - 8;
-  var top_y = this.y - 8;
-  var bottom_x = this.x + 8;
-  var bottom_y = this.y + 8;
+  var top_x = this.x - 4;
+  var top_y = this.y - 4;
+  var bottom_x = this.x + 4;
+  var bottom_y = this.y + 4;
 
-  if (this.y - 8 < 0) {
-      this.y = 8;
+  if (this.y - 10 < 0) {
+      this.y = 10;
       this.y_speed = -this.y_speed;
-  } else if (this.y + 8 > 500) {
-      this.y = 492;
+  } else if (this.y + 10 > 600) {
+      this.y = 590;
       this.y_speed = -this.y_speed;
   }
 
-  if (this.x < 0 || this.x > 600) {
+  if (this.x < 0 || this.x > 800) {
       this.x_speed = 3;
       this.y_speed = 1;
-      this.y = 200;
-      this.x = 300;
+      this.y = 300;
+      this.x = 400;
   }
-
-  if (top_x > 300) {
-      if (top_x < (paddle2.x + paddle2.height) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.width) && bottom_y > paddle2.y) {
-          this.x_speed = 3;
+  if (top_x > 400) {
+      if (top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
+          this.x_speed = -3;
           this.y_speed += (paddle2.y_speed / 2);
           this.x += this.x_speed;
       }
   } else {
-      if (top_x < (paddle1.x + paddle1.height) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.width) && bottom_y > paddle1.y) {
+      if (top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
           this.x_speed = 3;
           this.y_speed += (paddle1.y_speed / 2);
           this.x += this.x_speed;
